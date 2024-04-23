d96a76bfa806397eb5215b90f590b4e18c0dc291
Alessandro Pierro <info@alessandropierro.it>
2024-04-23 15:50:17 +0200

    feat: update charts path

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 helm package helm/railsk8s
 mv railsk8s* daniels_charts
-helm repo index daniels_charts --url https://bitbucket.org/modefinance/k8s_simah/src/main/daniels_charts/
+helm repo index daniels_charts --url https://modefinance.github.io/k8s_simah/daniels_charts/
