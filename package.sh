6cbabc2ded35736b7079596f2a7469cd6503102b
Alessandro Pierro <info@alessandropierro.it>
2024-04-24 13:52:11 +0200

    feat: rename daniels_charts to modefinance_charts

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 helm package helm/railsk8s
-mv railsk8s* daniels_charts
-helm repo index daniels_charts --url https://modefinance.github.io/k8s_simah/daniels_charts/
+mv railsk8s* modefinance_charts
+helm repo index modefinance_charts --url https://modefinance.github.io/k8s_simah/modefinance_charts/
